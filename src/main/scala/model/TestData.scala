4337722a5a3532175ecf9dc2bee85d3b89eafab6
Seiya Muramatsu <muramatsu_seiya@cyberagent.co.jp>
2017-05-07 23:22:26 +0900

    data周りを作成

---
+++ src/main/scala/model/TestData.scala
@@ -0,0 +1,15 @@
+package model
+
+// 各フィールドのの説明は TrainData.scala を参照
+case class TestData(
+  uid: String,
+  hour: Int,
+  advertiserId: Int,
+  campaignId: Int,
+  adId: Int,
+  siteId: Int,
+  c1: Int,
+  c2: Int,
+  n1: Double,
+  n2: Double
+)
---
+++ src/main/scala/model/TrainData.scala
@@ -0,0 +1,15 @@
+package model
+
+case class TrainData(
+  label: Double,      // 正解ラベル (0.0: クリックしていない, 1.0: クリックした)
+  uid: String,        // cookie等のuid
+  hour: Int,          // 広告接触した時間 (0 〜 23)
+  advertiserId: Int,  // 広告主ID (1 〜 10)
+  campaignId: Int,    // キャンペーンID (1 〜 50)
+  adId: Int,          // 広告ID (1 〜 100)
+  siteId: Int,        // サイトID (1 〜 100)
+  c1: Int,            // anonymized categorical variable1 (1 〜 100)
+  c2: Int,            // anonymized categorical variable2 (1 〜 100)
+  n1: Double,         // anonymized numerical variable1
+  n2: Double          // anonymized numerical variable2
+)
