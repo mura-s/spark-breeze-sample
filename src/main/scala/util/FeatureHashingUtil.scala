3f3d388b66bc03f0c7647771cbfbdf9673a57f68
Seiya Muramatsu <muramatsu_seiya@cyberagent.co.jp>
2017-05-07 23:22:54 +0900

    utilを作成

---
+++ src/main/scala/util/FeatureHashingUtil.scala
@@ -0,0 +1,19 @@
+package util
+
+import scala.util.hashing.MurmurHash3
+
+object FeatureHashingUtil {
+
+  val HashSize: Int = Math.pow(2.0, 15.0).toInt
+
+  def indexOf(value: String): Int = {
+    def nonNegativeMod(v: Int, mod: Int): Int = {
+      val rawMod = v % mod
+      rawMod + (if (rawMod < 0) mod else 0)
+    }
+
+    val hash = MurmurHash3.stringHash(value)
+    nonNegativeMod(hash, HashSize)
+  }
+
+}
---
+++ src/main/scala/util/OneHotEncodeUtil.scala
@@ -0,0 +1,8 @@
+package util
+
+object OneHotEncodeUtil {
+
+  def indexOf(value: Int, numFeatures: Int): Option[Int] =
+    if (value < numFeatures) Some(value) else None
+
+}
